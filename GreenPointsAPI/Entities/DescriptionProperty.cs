e1af7649e69fbaf25afa66b5f0d2fdd4cd3b48ee
boguminillo <boguminillo@gmail.com>
2023-02-23 10:36:59 +0100

    Proyecto API creado

---
+++ GreenPointsAPI/Entities/DescriptionProperty.cs
@@ -0,0 +1,7 @@
+﻿namespace GreenPointsAPI.Entities;
+
+public class DescriptionProperty
+{
+    public string Name { get; set; }
+    public string Description { get; set; }
+}
---
+++ GreenPointsAPI/Entities/GreenPoint.cs
@@ -0,0 +1,10 @@
+﻿namespace GreenPointsAPI.Entities;
+
+public class GreenPoint
+{
+
+    public string Coords { get; set; }
+    public string Name { get; set; }
+    public List<DescriptionProperty> Properties { get; set; }
+
+}
