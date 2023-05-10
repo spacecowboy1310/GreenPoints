57e8d954b81528054006b6bbc682758a30042183
Txanrru <oier1310@gmail.com>
2023-05-10 23:03:40 +0200

    feature: delete extra field

--- GreenPoint/wwwroot/css/script.js
+++ GreenPoint/wwwroot/css/script.js
@@ -1,10 +1,24 @@
 function initializeMap(token) {
     mapboxgl.accessToken = token;
 
-    var map = new mapboxgl.Map({
+    const map = new mapboxgl.Map({
         container: 'map',
         style: 'mapbox://styles/mapbox/streets-v11',
         center: [-2.9429086, 43.2633976], // Longitude, Latitude
         zoom: 12
     });
+
+    map.on('style.load', function () {
+        map.on('click', function (e) {
+            var coordinates = e.lngLat;
+
+            document.getElementById('inputLatitud').value = coordinates.lat
+            document.getElementById('inputLongitud').value = coordinates.lng;
+            //new mapboxgl.Popup()
+            //    .setLngLat(coordinates)
+            //    .setHTML('you clicked here: <br/>' + coordinates)
+            //    .addTo(map);
+        });
+    });
 }
+
